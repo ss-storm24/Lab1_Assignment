3204443b0db5c9edacb95992e62167b5004e9916
ss-storm24 <sandradina.yimbo@outlook.com>
2022-02-07 21:17:50 -0700

    Updated commit

--- import.py
+++ import.py
@@ -1,8 +1,8 @@
 import os
 import csv
 
-from sqalchemy import create_engine
-from sqalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
@@ -12,7 +12,7 @@ def main():
     reader = csv.reader(f_books)
     for title, author, year, isbn in reader:
         db.execute("INSERT INTO books (title, author, year, isbn) VALUES (:title, :author, :year, :isbn)", {"title": title, "author": author, "year": year, "isbn": isbn})
-        print(f_books"Added book with title:{title}, author:{author}, publication year:{year}, isbn:{isbn}")
+        print(f"Added book with title:{title}, author:{author}, publication year:{year}, and isbn:{isbn}")
     db.commit()
 
 if __name__=="__main__":
